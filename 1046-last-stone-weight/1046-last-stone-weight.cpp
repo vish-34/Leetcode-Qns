6169e408d0d1044c524e6e1b233a363c546aaf05
Vishal Borana <vishal23borana@gmail.com>
2026-07-09 13:27:39 +0530

    Time: 0 ms (100%), Space: 10.1 MB (13.12%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+
+        priority_queue<int> pq;
+
+        for (int stone : stones) {
+            pq.push(stone);
+        }
+
+        while (pq.size() > 1) {
+
+            int first = pq.top();
+            pq.pop();
+
+            int second = pq.top();
+            pq.pop();
+
+            if (first != second) {
+                pq.push(first - second);
+            }
+        }
+
+        return pq.empty() ? 0 : pq.top();
+    }
+};
