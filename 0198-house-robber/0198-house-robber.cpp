1ca4f9c606ad08b836936bb8bf85fddb8da5b6d8
Vishal Borana <vishal23borana@gmail.com>
2026-07-24 08:44:36 +0530

    Time: 0 ms (100%), Space: 10 MB (90.01%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)
+        return nums[0];
+
+        int prev2 = nums[0];
+        int prev1 = max(nums[0], nums[1]);
+
+        for( int i = 2; i < n; i++){
+            int curr = max(prev1, nums[i] + prev2);
+
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+};
