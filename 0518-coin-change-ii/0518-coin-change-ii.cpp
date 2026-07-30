46fdcf80799870dfff9e00c7866e8ecf9496e304
Vishal Borana <vishal23borana@gmail.com>
2026-07-30 08:35:41 +0530

    Time: 3 ms (98.65%), Space: 10.4 MB (86.66%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+
+        vector< unsigned long long> dp(amount + 1, 0);
+
+        dp[0] = 1;
+
+        for (int coin : coins) {
+
+            for (int i = coin; i <= amount; i++) {
+
+                dp[i] += dp[i - coin];
+            }
+        }
+
+        return dp[amount];
+    }
+};
