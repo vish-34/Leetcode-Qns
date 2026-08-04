5c117dfbd3f8d5b880de564e64f54f40db009fc3
Vishal Borana <vishal23borana@gmail.com>
2026-08-04 21:24:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (26.18%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[0] = 1;
+        dp[1] = (s[0] != '0');
+        for(int i = 2; i <= n; i++){
+            if(s[i-1] != '0')
+            dp[i] += dp[i-1];
+
+            int two = stoi(s.substr(i-2, 2));
+            if(two >= 10 && two <= 26)
+            dp[i] += dp[i-2];
+        }
+        return dp[n];
+    }
+};
